77610cc31394446cb00c9c8ec0a70ec914e799ee
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2026-02-04 22:40:39 +0530

    add patient service

--- patient-service/src/main/java/com/hospital/patient_service/entity/Patient.java
+++ patient-service/src/main/java/com/hospital/patient_service/entity/Patient.java
@@ -1,4 +1,58 @@
 package com.hospital.patient_service.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "patients")
+@EntityListeners(AuditingEntityListener.class)
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private Date dateOfBirth;
+
+    private String bloodGroup;
+
+    //Auto generated
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+
 }
